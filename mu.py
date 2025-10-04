3bb3ab163868aee60b6ac25aa45c77ddaed33d4c
bbyashi <chikubhardwaj1525@gmail.com>
2025-10-04 23:05:21 +0530

    Delete mu.py

--- mu.py
+++
@@ -1,9 +0,0 @@
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from telegram import Update
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Bot is working!")
-
-app = ApplicationBuilder().token("8388314171:AAFXrRKZU0d7XMRP5sRNi89ixXXzYGo0_Ws").build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
